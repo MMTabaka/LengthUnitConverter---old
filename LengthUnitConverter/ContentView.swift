3dda88323cd514c5d1474b8a4e6a242d849012f7
MTabaka <miloszmtabaka@gmail.com>
2022-02-14 20:19:48 +0000

    Added keyboard with a focus

--- LengthUnitConverter/ContentView.swift
+++ LengthUnitConverter/ContentView.swift
@@ -11,34 +11,54 @@ struct ContentView: View {
     @State private var value1: Double = 5
     @State private var selectedUnit1 = "meter"
     @State private var selectedUnit2 = "meter"
+    @FocusState private var amountIsFocused: Bool
     
     var convertedValue: Double {
-        return value1 * universalUnit[selectedUnit1]! / universalUnit[selectedUnit2]!
+        value1 * universalUnit[selectedUnit1]! / universalUnit[selectedUnit2]!
     }
     
     let units = ["meter", "km", "foot", "yard", "mile"]
     let universalUnit = [ "meter": 1.0, "km": 1000.0, "foot": 0.30479924241959999165, "yard":0.9143977272588, "mile": 1609.34]
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Unit one", value: $value1, format: .number)
-                Picker("Selected unit 1", selection: $selectedUnit1) {
-                    ForEach(units, id: \.self) {
-                        Text($0)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter your value...", value: $value1, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Selected unit 1", selection: $selectedUnit1) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
                     }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Initial Value")
                 }
-                .pickerStyle(.segmented)
+                
+                Section {
+                    Text(convertedValue, format: .number)
+                    Picker("Selected unit 2", selection: $selectedUnit2) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Converted Value")
+                }
             }
-            
-            Section {
-                Text(convertedValue, format: .number)
-                Picker("Selected unit 2", selection: $selectedUnit2) {
-                    ForEach(units, id: \.self) {
-                        Text($0)
+            .navigationTitle("Unit Converter")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
                     }
                 }
-                .pickerStyle(.segmented)
             }
         }
     }

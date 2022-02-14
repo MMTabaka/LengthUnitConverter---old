7e830e6213eef4e1a48927a8a081d90bafacb996
MTabaka <miloszmtabaka@gmail.com>
2022-02-14 19:29:59 +0000

    Initial Commit

---
+++ LengthUnitConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LengthUnitConverter
+//
+//  Created by Milosz Tabaka on 14/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LengthUnitConverter/LengthUnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthUnitConverterApp.swift
+//  LengthUnitConverter
+//
+//  Created by Milosz Tabaka on 14/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct LengthUnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
